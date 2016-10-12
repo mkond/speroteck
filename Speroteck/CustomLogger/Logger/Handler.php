4689dd504260b9c966887e6f18cba53506bef9dc
MykhailoKondrashyna <mkondrashyna@speroteck.com>
2016-10-12 10:23:24 +0300

    chenged logger

--- Speroteck/CustomLogger/Logger/Handler.php
+++ Speroteck/CustomLogger/Logger/Handler.php
@@ -4,8 +4,10 @@ namespace Speroteck\CustomLogger\Logger;
 //use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\LineFormatter;
+use Magento\Framework\App\Action\Context;
+use \Magento\Framework\App\Action\Action;
 
-class Handler extends \Magento\Framework\Logger\Handler\Base
+class Handler extends \Magento\Framework\App\Action\Action
 {
     /**
      * Logging level
@@ -18,4 +20,19 @@ class Handler extends \Magento\Framework\Logger\Handler\Base
      * @var string
      */
     protected $fileName = '/var/log/custom.log';
+
+    public function __construct(
+        \Magento\Customer\Model\Visitor $customerVisitor
+    ){
+        $this->_customerVisitor = $customerVisitor;
+
+    }
+    public function execute()
+    {
+       // $this->_customerVisitor = $customerVisitor;
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
+        $logger = new \Zend\Log\Logger();
+        $logger->addWriter($writer);
+        $logger->info('Your text message');;
+    }
 }
